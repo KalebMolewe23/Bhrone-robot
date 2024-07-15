03b934c48a177bfe6517b0e196550394e1792a2b
kaleb <88307946+KalebMolewe23@users.noreply.github.com>
2024-07-15 19:53:29 +0700

    menu admin

--- resources/views/admin/notes/edit.blade.php
+++ resources/views/admin/notes/edit.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Edit Artikel</title>
+    <link rel="icon" href="{{ asset('assets/image/icon.png') }}" type="image/x-icon">
 
     <!-- css style -->
     <link rel="stylesheet" href="{{ asset('assets/frontend/css/style.css') }}">
--- resources/views/admin/notes/index.blade.php
+++ resources/views/admin/notes/index.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data Artikel</title>
+    <link rel="icon" href="{{ asset('assets/image/icon.png') }}" type="image/x-icon">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
